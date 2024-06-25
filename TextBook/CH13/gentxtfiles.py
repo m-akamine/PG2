c375ad6db4ef0f02d221c7ea9bd91ca7b49ddeb8
m-akamine <m-akamine@std.it-college.ac.jp>
2024-06-25 02:51:10 +0000

    CH13 修正

--- TextBook/CH13/gentxtfiles.py
+++ TextBook/CH13/gentxtfiles.py
@@ -4,10 +4,10 @@
 for i in range(1, 10):
     filename = 'spam{:03}.txt'.format(i)
     with open(filename, 'w', encoding='utf-8') as text_file:
-        text_file.write(f'''これは
+        text_file.write(f'''This is
 {filename}
-というテキストファイルです。
+File Text.
 ''')
         for j in range(1, i + 1):
-            text_file.write(f'適当なデータ{j}\n')
-        text_file.write('最終行')
+            text_file.write(f'Appropriate data{j}\n')
+        text_file.write('last line')
